ab68b0aaae15203072dbdeb60e2788ffdebeb17c
brightio <65655412+brightio@users.noreply.github.com>
2021-06-05 22:55:33 +0200

    Upload extras

---
+++ extras/upgrade-bash.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+WAIT=5
+
+echo "Go to the UNIX shell that needs upgrade"
+
+while [ $WAIT -gt 0 ]
+do
+echo Seconds left: "$WAIT"
+sleep 1
+WAIT=`expr $WAIT - 1`
+done
+
+ROWS=`tput lines`
+COLUMNS=`tput cols`
+
+xdotool type $'python -c "import pty; pty.spawn(\'/bin/bash\')"'
+xdotool key Return
+xdotool type 'export TERM=xterm-256color'
+xdotool key Return
+xdotool type 'export SHELL=/bin/bash'
+xdotool key Return
+xdotool key ctrl+z
+xdotool type 'stty raw -echo;fg'
+xdotool key Return
+xdotool type "stty rows $ROWS columns $COLUMNS"
+xdotool key Return
+xdotool type reset
+xdotool key Return
+
